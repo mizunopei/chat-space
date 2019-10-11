8824f4d9bd17208deb8614ff9c93cbc2ace78a88
mizunopei <m.shuhei0309@gmail.com>
2019-10-11 17:31:43 +0900

    add animate of method, append

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,32 @@
 $(function(){
   function buildHTML(message){
-    var html = `.message
-                  .upper-message
-                    .upper-message__user-name
-                      ${ message.user.name }
-                    .upper-message__date
-                      ${ message.created_at.strftime("%Y/%m/%d %H:%M")}
-                  .lower-message
-                    - if message.content.present?
-                      %p.lower-message__content
-                        = ${ message.content }
-                      = image_tag ${ message.image }.url, class: 'lower-message__image' if message.image.present?`
+    var html = `<div class = "message">
+                  <div class = "upper-message">
+                    <div class = "upper-message__user-name">
+                      ${ message.user_name }
+                    </div>
+                    <div class = "upper-message__date">
+                      ${ message.time}
+                    </div>
+                  </div>
+                  <div class = "lower-message">
+                      <p class = "lower-message__content" >
+                        ${ message.content} 
+                      </p>
+                      <img class = "lower-message__image" ${ message.image }.url>
+                  </div>
+                </div>`
     return html;
    }
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
+
     $.ajax({
       url: url,
       type: "POST",
+      dataType: "json",
       data: formData,
       processData: false,
       contentType: false
@@ -28,6 +35,13 @@ $(function(){
       var html = buildHTML(data);
       $('.messages').append(html)
       $('.form_message').val('')
+      $('.messages').animate({
+        scrollTop: $('.messages')[0].scrollHeight
+      });
+      return false
+    })
+    .fail(function(){
+      alert('error');
     })
   })
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.user_name @message.user.name
 json.content   @message.content
 json.image     @message.image
-json.time      @message.created_at
+json.time      @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.id        @message.id
