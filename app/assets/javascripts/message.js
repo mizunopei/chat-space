0a4109f3c5505a8b7bd126303091bb79a7f3165a
mizunopei <m.shuhei0309@gmail.com>
2019-10-11 13:57:26 +0900

    add done

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,17 @@
 $(function(){
+  function buildHTML(message){
+    var html = `.message
+                  .upper-message
+                    .upper-message__user-name
+                      ${ message.user.name }
+                    .upper-message__date
+                      ${ message.created_at.strftime("%Y/%m/%d %H:%M")}
+                  .lower-message
+                    - if message.content.present?
+                      %p.lower-message__content
+                        = ${ message.content }
+                      = image_tag ${ message.image }.url, class: 'lower-message__image' if message.image.present?`
+   }
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -9,6 +22,11 @@ $(function(){
       data: formData,
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.form_message').val('')
     })
   })
 })
