beb8d28844d2a08c0a9fc9c2b64f145dec2aa509
mizunopei <m.shuhei0309@gmail.com>
2019-10-11 13:04:53 +0900

    add respond_to

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,8 +3,8 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-    $ajax({
-      url: url;
+    $.ajax({
+      url: url,
       type: "POST",
       data: formData,
       processData: false,
